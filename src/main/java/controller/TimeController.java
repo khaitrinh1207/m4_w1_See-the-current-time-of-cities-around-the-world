90a4a99fd66356cdfea73a1a483b9903fbcc7981
khaitrinh1207 <khaitrinh1207@gmail.com>
2021-03-07 12:45:12 +0700

    show time

---
+++ src/main/java/controller/TimeController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+@Controller
+@RequestMapping("worldclock")
+public class TimeController {
+
+    @GetMapping
+    public ModelAndView getTimeByTimezone(@RequestParam(name = "city", required = false, defaultValue = "Asia/Ho_Chi_Minh") String city) {
+        ModelAndView modelAndView = new ModelAndView("index");
+        // Get current time at local
+        Date date = new Date();
+// Get timezone by the local
+        TimeZone local = TimeZone.getDefault();
+// Get timezone by the specified city
+        TimeZone locale = TimeZone.getTimeZone(city);
+        // Calculate the current time in the specified city
+        long locale_time = date.getTime() +
+                (locale.getRawOffset() - local.getRawOffset());
+// Reset the date by locale_time
+        date.setTime(locale_time);
+        modelAndView.addObject("city", city);
+        modelAndView.addObject("date", date);
+        return modelAndView;
+    }
+}
